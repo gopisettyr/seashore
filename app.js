baed64f5a4b120788446ea3ad77441af22b7bb84
Tekumalla Sai Sriram <tekumallasaisriram@gmail.com>
2019-03-04 18:29:57 +0530

    try

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyPaser.urlencoded({extended : false}));
 
 app.get('/', (req,res,next) => {
-    res.sendFile(path.join(__dirname + 'index.html'));
+    res.sendFile(path.join(__dirname + './index.html'));
 })
 
 app.listen(3000);
