5404a128488b5f50168778128e29f6ce2872b5ed
Tekumalla Sai Sriram <tekumallasaisriram@gmail.com>
2019-03-04 19:15:05 +0530

    All on Node

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # seashore
 This is a practice site which worked with @TSaiSriram
+
+Node is'nt working!!
--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const bodyPaser = require('body-parser');
 const path = require('path');
-
-app.use(express.static(path.join(__dirname , 'public')));
 app.use(bodyPaser.urlencoded({extended : false}));
-
+app.use(express.static(path.join(__dirname , 'public')))
 app.get('/', (req,res,next) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
