a48938581ea23f1398a747479fb0223a2bf979de
Tekumalla Sai Sriram <tekumallasaisriram@gmail.com>
2019-03-04 17:12:10 +0530

    All server

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const bodyPaser = require('body-parser');
 const path = require('path');
-app.use(express.static(path.join('public')))
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyPaser.urlencoded({extended : false}));
 
 app.get('/', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + 'index.html'));
 })
 
 app.listen(3000);
