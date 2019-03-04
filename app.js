2d73238962abd2feb5977919c819b67699bb05d1
Tekumalla Sai Sriram <tekumallasaisriram@gmail.com>
2019-03-04 15:18:13 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
 const bodyPaser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
 
+app.use(bodyPaser.urlencoded({extended : false}));
 
+app.get('/', (req,res,next) => {
+    res.sendFile(path.join(__dirname + './index.html'));
+})
+
+app.listen(3000);
