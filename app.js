55c65a1a8258e695923defa56cec2a2a6d95de55
Tekumalla Sai Sriram <tekumallasaisriram@gmail.com>
2019-03-04 21:29:28 +0530

    final again

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const bodyPaser = require('body-parser');
 const path = require('path');
+const PORT = process.env.PORT || 5000
+
 app.use(bodyPaser.urlencoded({extended : false}));
 app.use(express.static(path.join(__dirname , 'public')))
 app.get('/', (req,res,next) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.listen(5000);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
