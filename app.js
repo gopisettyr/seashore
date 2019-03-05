fa44c61f6c9ed95f7aff1f67741b438f8b3a21fe
Tekumalla Sai Sriram <tekumallasaisriram@gmail.com>
2019-03-05 15:22:41 +0530

    Mail working initail setup

--- app.js
+++ app.js
@@ -6,20 +6,26 @@ const PORT = process.env.PORT || 3000
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 
-const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport(
     sendgridTransport({
         auth : {
             api_key : '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'
         }
     })
-});
+);
 
 app.use(bodyPaser.urlencoded({extended : false}));
 app.use(express.static(path.join(__dirname , 'public')))
 app.get('/', (req,res,next) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
-app.post('/', (req,res,next) => {
-
+app.post('/mail', (req,res,next) => {
+    res.redirect('/');
+    return transporter.sendMail({
+        to: req.body.email,
+        from: 'support@starkexpo.com',
+        subject: 'Signup succeeded!',
+        html: '<h1>You successfully Subscribed for Seashore services and you will get new updates ASAP....!!</h1>'
+      });
 })
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
